14342a129733eee4a2488c86c528b8b110052f69
Hannah Chen <z5209534@ad.usw.edu.au>
2018-07-27 18:32:44 +1000

    complete

--- strings.py
+++ strings.py
@@ -1,2 +1,9 @@
-strings = ['This', 'list', 'is', 'now', 'all', 'together']
-sentence = ''
+strings = ['This', 'list', 'is', 'now', 'all', 'together'] 
+sentence = ' '
+i = 0
+sentence = 'This'
+for i in strings[1:]:
+ 	sentence = sentence+ ' ' + i
+print(sentence)
+print(' '.join(strings))
+
